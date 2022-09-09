21b1d08bb731d973a0f62a50467ed126d9d6e8e8
tamar09 <112966258+tamar09@users.noreply.github.com>
2022-09-08 23:02:32 -0500

    Update Main.java
    modificado por la web

--- Hello/src/Main.java
+++ Hello/src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("cambio enviado desde intellij");
+        //cambio modificado a la web
+        for(int i=0;i<10;i++){
+            System.out.println("holii");
+        }
     }
 }
