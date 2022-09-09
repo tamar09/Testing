5d4fa9a3b84c5613186e5ab1e85d1f75256ec840
tamaralondra54321 <tamaralondra54321@gmail.com>
2022-09-08 22:58:40 -0500

    cambio desde intellij

--- Hello/src/Auto.java
+++ Hello/src/Auto.java
@@ -1,3 +1,4 @@
 public class Auto {
 
+
 }
--- Hello/src/Main.java
+++ Hello/src/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
         System.out.println("Hello world!");
 
-
         System.out.println("cambio enviado desde intellij");
     }
 }
